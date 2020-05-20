737c54e84aa835699e8afeadf6ffe5c35ff2a83a
Alexander Frolov <frolov@sharp-dev.net>
2020-05-20 14:44:58 +0500

    added user-service project

---
+++ user-service/src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { MessagePattern } from '@nestjs/microservices';
+import { of } from 'rxjs';
+import { delay } from "rxjs/operators";
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @MessagePattern({ cmd: "ping" })
+  ping(_: any) {
+    return of("pong").pipe(delay(1000));
+  }
+}
